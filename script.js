8ffb8e01ac5cc9c51fd4272cdbe2ed37a4c03a65
Brian-Lu <brianlu22@yahoo.com>
2017-03-10 03:13:21 -0500

    Got it to work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var svg = document.getElementById("svg");
 var NS = "http://www.w3.org/2000/svg";
+var move_circles = false;
 
 function getCursorPosition(canvas, event) {
     var rect = canvas.getBoundingClientRect();
@@ -7,34 +8,110 @@ function getCursorPosition(canvas, event) {
     var y = event.clientY - rect.top;
     var ret = [x,y];
     return ret;
-}
+};
 
 var DEATH = function(e) {
 //    e.stopPropagation();
     svg.removeChild(this);
     console.log("ANOTHER ONE BITES THE DUST");
-    x = Math.floor(Math.random()*svg.getBoundingClientRect().width);
-    y = Math.floor(Math.random()*svg.getBoundingClientRect().height);
-    svg.appendChild(make_circ(x,y));
+    var x = Math.floor(Math.random()*svg.getBoundingClientRect().width);
+    var y = Math.floor(Math.random()*svg.getBoundingClientRect().height);
+    var new_circ = make_circ(x, y);
+    svg.appendChild(new_circ);
+    var xdirection = false;
+    var ydirection = false;
+    var randx = Math.random() * 2 + 1;
+    var randy = Math.random() * 2 + 1;
+    var move_circle1 = function() {
+        if(move_circles) {
+            new_circ.setAttribute("cx", x);
+            new_circ.setAttribute("cy", y);
+            if (xdirection) {
+                x -= randx;
+            }
+            else {
+                x += randx;
+            }
+            if (ydirection) {
+                y -= randy;
+            }
+            else {
+                y += randy;
+            }
+            if (x <= 0) {
+                xdirection = false;
+            }
+            if (x >= 640) {
+                xdirection = true;
+            }
+            if (y <= 0) {
+                ydirection = false;
+            }
+            if (y >= 480) {
+                ydirection = true;
+            }
+        }
+        window.requestAnimationFrame(move_circle1)
+    };
+    move_circle1();
+
     console.log("BIRTH");
-}
+};
 
 var click_circ = function(e) {
     e.stopPropagation();
     this.setAttribute("fill","pink");
     this.addEventListener("click", DEATH);
     console.log("IRRITATED");
-}
+};
 
 var draw_circ = function(e) {
     var coor = getCursorPosition(svg, e);
-    svg.appendChild(make_circ(coor[0],coor[1]));
+    var new_circ = make_circ(coor[0], coor[1]);
+    svg.appendChild(new_circ);
     console.log("DRAW");
-}
+    var x = e.offsetX ;
+    var y = e.offsetY ;
+    var xdirection = false;
+    var ydirection = false;
+    var randx = Math.random() * 2 + 1;
+    var randy = Math.random() * 2 + 1;
+    var move_circle = function() {
+        if(move_circles) {
+            new_circ.setAttribute("cx", x);
+            new_circ.setAttribute("cy", y);
+            if (xdirection) {
+                x -= randx;
+            }
+            else {
+                x += randx;
+            }
+            if (ydirection) {
+                y -= randy;
+            }
+            else {
+                y += randy;
+            }
+            if (x <= 0) {
+                xdirection = false;
+            }
+            if (x >= 640) {
+                xdirection = true;
+            }
+            if (y <= 0) {
+                ydirection = false;
+            }
+            if (y >= 480) {
+                ydirection = true;
+            }
+        }
+        window.requestAnimationFrame(move_circle)
+    };
+    move_circle();
+};
 
 var make_circ = function(x,y) {
     var c = document.createElementNS(NS, "circle");
-    var line = document.createElementNS(NS, "line");    
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
     c.setAttribute("fill","white");
@@ -53,14 +130,12 @@ clearbtn.addEventListener("click", function(e){
 	svg.removeChild(svg.lastChild);
     }
     console.log("ERADICATION");
+    move_circles = false;
 });
 
-var move_circles = function(e) {
-    var listOfCircles = document.getElementsByTagName('circle');
-    for (i = 0; i < listOfCircles.length; i++){
-	console.log(listOfCircles[i]);
-    }
+var get_jiggy_with_it = function(e) {
+    move_circles = true;
 };
 
 var movebtn = document.getElementById("move");
-movebtn.addEventListener("click", move_circles);
+movebtn.addEventListener("click", get_jiggy_with_it);
